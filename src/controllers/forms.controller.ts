b70a06d3d6b73214158fa86d49e2289a8d82197c
Kendrick9470 <odediranifeoluwa7@gmail.com>
2025-02-22 16:18:29 +0100

    create form route

--- src/controllers/forms.controller.ts
+++ src/controllers/forms.controller.ts
@@ -32,3 +32,49 @@ export const createForm = async (
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+export const assignUser = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { userId, formId } = req.body;
+
+  try {
+    // Check if the user and form exist
+    const userExists = await prisma.user.findUnique({ where: { id: userId } });
+    const formExists = await prisma.form.findUnique({ where: { id: formId } });
+
+    if (!userExists || !formExists) {
+      res.status(404).json({ error: "User or Form not found" });
+    }
+
+    // Check if assignment already exists
+    const existingAssignment = await prisma.assignedForm.findUnique({
+      where: { userId_formId: { userId, formId } },
+    });
+
+    if (existingAssignment) {
+      res.status(400).json({ error: "User already assigned to this form" });
+    }
+
+    // Assign user to form
+    const assignedForm = await prisma.assignedForm.create({
+      data: { userId, formId },
+    });
+
+    res
+      .status(201)
+      .json({ message: "User assigned successfully", assignedForm });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getAssignedUser = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+  } catch (error) {}
+};
--- src/routes/forms.route.ts
+++ src/routes/forms.route.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import { createForm } from "../controllers/forms.controller";
+import { assignUser, createForm } from "../controllers/forms.controller";
 
 const router = express.Router();
 
 router.route("/create").post(createForm);
 
-router.route("/code").post(() => {});
+router.route("/assign-user").post(assignUser);
 
 export default router;
